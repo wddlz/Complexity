425ecbedc8718972f49ea6ba791a3aa0f0fd8041
Ian Drosos <ian.drosos@gmail.com>
2016-09-24 11:51:56 -0400

    test auto build

--- analysis.js
+++ analysis.js
@@ -44,7 +44,7 @@ function ComplexityBuilder()
 		console.log(
 		   (
 		   	"{0}(): {1}\n" +
-		   	"============TESTdev\n" +
+		   	"============TESTINGNEWDEVSTUFF\n" +
 			   "SimpleCyclomaticComplexity: {2}\t" +
 				"MaxNestingDepth: {3}\t" +
 				"MaxConditions: {4}\t" +
